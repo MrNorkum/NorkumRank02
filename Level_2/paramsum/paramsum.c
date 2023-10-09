23966f4514227e072c3f10a79a98db6865181240
Mr. Norkum <kelestayyib@gmail.com>
2023-10-09 14:32:39 +0300

    Successful

--- Level_2/paramsum/paramsum.c
+++ Level_2/paramsum/paramsum.c
@@ -1,13 +1,9 @@
 #include <unistd.h>
 
-void	putnbr(int n)
-{
-	if (n > 10)
-		putnbr(n / 10);
-	write(1, &"0123456789"[n % 10], 1);
-}
-
 int	main(int ac, char **av)
 {
-	return ((void)av, putnbr(ac), write(1, "\n", 1), 0);
+	if (ac > 9)
+		write(1, &"0123456789"[ac / 10], 1);
+	write(1, &"0123456789"[ac % 10], 1);
+	return ((void)av, write(1, "\n", 1), 0);
 }
