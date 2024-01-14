5cf0c83f45e1a9206ca4a49c6b29ba2f927e10c5
Mr. Norkum <kelestayyib@gmail.com>
2024-01-14 05:13:04 +0300

    Successful

--- Level_2/hidenp/hidenp.c
+++ Level_2/hidenp/hidenp.c
@@ -4,16 +4,15 @@ int		main(int ac, char **av)
 {
 	if (ac == 3)
 	{
-		while (*av[1])
+		while (*av[2])
 		{
-			while (*av[2] && *av[1] != *av[2])
-				av[2]++;
-			if (*av[2])
-				return (write(1, "0\n", 2), 0);
-			av[1]++;
+			if (*av[1] == *av[2])
+				av[1]++;
 			av[2]++;
 		}
-		return ((1, "1\n", 2), 0);
+		if (*av[1] == '\0')
+			return (write(1, "1\n", 2), 0);
+		return (write(1, "0\n", 2), 0);
 	}
 	return ((1, "\n", 1), 0);
 }
