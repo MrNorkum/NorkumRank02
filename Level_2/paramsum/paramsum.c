51d3509b7d60a6f7a5be022dccda40a8350705b9
Mr. Norkum <kelestayyib@gmail.com>
2024-01-14 05:22:35 +0300

    Successful

--- Level_2/paramsum/paramsum.c
+++ Level_2/paramsum/paramsum.c
@@ -2,7 +2,7 @@
 
 int	main(int ac, char **av)
 {
-	if (ac > 9)
+	if (ac-- > 9)
 		write(1, &"0123456789"[ac / 10], 1);
 	write(1, &"0123456789"[ac % 10], 1);
 	return ((void)av, write(1, "\n", 1), 0);
